03870c9fa464a9618b120f51aa4b7d42e3557ddc
unknown <winstonshred@gmail.com>
2020-11-25 23:59:15 +1300

    added tilemap and initialized player

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -6,8 +6,14 @@ export class BootScene extends Phaser.Scene {
 
     preload() {
         // Holds all assets (sprites, sound)
-        this.load.image('sprite', 'assets/sprites/sprite.png')
-        this.load.image('sprite2', 'assets/sprites/sprite2.png')
+        // this.load.image('sprite', 'assets/sprites/sprite.png')
+        // this.load.image('sprite2', 'assets/sprites/sprite2.png')
+
+        // preload tileset asset
+        this.load.image("tiles", "assets/tilemaps/tiles/firstTileset.png")
+        // preload tilemap asset
+        this.load.tilemapCSV("tilemap", "assets/tilemaps/maps/tilemap1.csv" )
+
     }
 
     create() {
